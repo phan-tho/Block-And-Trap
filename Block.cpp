ce41cc6e1073b34a8c0a30a188935b36ece1ea54
Phan Tho <phantho.vt.vp@gmail.com>
2024-03-07 02:36:38 +0700

    fst commit

---
+++ Block.cpp
@@ -0,0 +1,100 @@
+#include "def.h"
+#include "LTexture.h"
+#include <iostream>
+
+LTexture gBackground;
+LTexture gBlock;
+
+struct Block{
+    const int BLOCK_HEIGHT = 10;
+    const int BLOCK_WIDTH  = 65;
+    
+    int Denta_Y = 3;
+    
+    int PosX, PosY;
+    
+    Block();
+    
+    void move();
+    
+    void end();
+    
+    void render();
+};
+
+Block::Block(){
+    PosY = FLOOR;
+    PosX = rand()%(rPIVOT - BLOCK_WIDTH - lPIVOT + 1) + lPIVOT;
+}
+
+void Block::move(){
+    PosY -= Denta_Y;
+}
+
+void Block::render(){
+    gBlock.render(PosX, PosY, NULL);
+}
+
+void Block::end(){
+    
+}
+
+
+int main( int argc, char* argv[] ){
+    init();
+    loadMedia();
+    
+    bool quit = false;
+    SDL_Event e;
+    
+    Block block;
+    
+    while( !quit ){
+        while( SDL_PollEvent( &e ) != 0 ){
+            if( e.type == SDL_QUIT || e.key.keysym.sym == SDLK_x)        quit = true;
+        }
+
+        //Clear screen
+        SDL_SetRenderDrawColor( gRenderer, 0x00, 0xFF, 0xFF, 0xFF );
+        SDL_RenderClear( gRenderer );
+
+        //Render objects
+        gBackground.render(0, 0, NULL);
+        
+        // move and render block
+        block.move();
+        block.render();
+        
+        
+        //Update screen
+        SDL_RenderPresent( gRenderer );
+    }
+    
+    close();
+
+    return 0;
+}
+
+void loadMedia(){
+    gBackground.loadFromFile("BackGr.png");
+    gBlock.loadFromFile("Block.png");
+}
+
+void close()
+{
+    //Free loaded images
+    gBackground.freeFire();
+    gBlock.freeFire();
+
+    //Destroy window
+    SDL_DestroyRenderer( gRenderer );
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+    gRenderer = NULL;
+
+    //Quit SDL subsystems
+    IMG_Quit();
+    SDL_Quit();
+}
+
+
---
+++ def.h
@@ -0,0 +1,48 @@
+#ifndef def_h
+#define def_h
+
+#include "SDL2/SDL.h"
+#include "SDL2_image/SDL_image.h"
+#include <string>
+#include <iostream>
+#include <random>
+
+const int SCREEN_WIDTH  = 428;           // 640
+const int SCREEN_HEIGHT = 748;          // 480
+
+const int CEILING = 90;
+const int FLOOR   = 666;
+const int lPIVOT  = 40;
+const int rPIVOT  = 390;
+
+SDL_Window* gWindow = NULL;
+SDL_Renderer* gRenderer = NULL;
+
+void init();
+void loadMedia();
+void close();
+
+void init(){
+    SDL_Init( SDL_INIT_VIDEO );
+    
+    //Create window
+    gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    
+    //Create vsynced renderer for window
+    gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+
+    //Initialize renderer color
+    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+    //Initialize PNG loading
+    IMG_Init(IMG_INIT_PNG);
+    
+    //Initialize SDL_ttf
+//    TTF_Init();
+}
+
+
+#endif /* def_h */
+
+
+
